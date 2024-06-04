f68aeeb1d98e586d75a83494f0bc236be043b0bb
Zack <zack@zackbradshaw.com>
2024-06-03 21:33:19 -0500

    feat: add openai example

---
+++ examples/openai_request.py
@@ -0,0 +1,35 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+# Load environment variables from .env file
+openai_api_base = os.getenv("OPENAI_API_BASE")
+openai_api_key = os.getenv("OPENAI_API_KEY")
+model = os.getenv("MODEL")
+
+client = OpenAI(
+    api_key=openai_api_key,
+    base_url=openai_api_base,
+)
+
+
+chat_response = client.chat.completions.create(
+    model=model,
+    messages=[{
+        "role": "user",
+        "content": [
+            # TODO Add vision proxy for horde
+            # Uncommment for vision
+            # {
+            #     "type": "image_url",
+            #     "image_url": {
+            #         "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg",
+            #     },
+            # },
+            {"type": "text", "text": "What's in this image?"},
+        ],
+    }],
+)
+
+print("Chat response:", chat_response)
